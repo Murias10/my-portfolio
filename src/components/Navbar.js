72561be0e996946b9288be6639675053d59325dd
Murias10 <uo290009@uniovi.es>
2024-12-23 16:41:09 +0100

    Update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,23 +1,31 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import './Navbar.css';
-
-const handleLinkClick = (e) => {
-    const links = document.querySelectorAll('.navbar-link');
-    links.forEach(link => link.classList.remove('active'));
-    e.target.classList.add('active');
-}
-
 
 function Navbar() {
     return (
         <nav className="navbar">
             <ul className="navbar-list">
                 <li className="navbar-item">
-                    <Link className="navbar-link active" to="" onClick={handleLinkClick}>About</Link>
+                    {/* Usamos NavLink para gestionar automáticamente la clase activa */}
+                    <NavLink
+                        className={({ isActive }) =>
+                            isActive ? 'navbar-link active' : 'navbar-link'
+                        }
+                        to="/"
+                    >
+                        About
+                    </NavLink>
                 </li>
                 <li className="navbar-item">
-                    <Link className="navbar-link" to="/projects" onClick={handleLinkClick}>Projects</Link>
+                    <NavLink
+                        className={({ isActive }) =>
+                            isActive ? 'navbar-link active' : 'navbar-link'
+                        }
+                        to="/projects"
+                    >
+                        Projects
+                    </NavLink>
                 </li>
             </ul>
         </nav>
